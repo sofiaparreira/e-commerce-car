6d6c836413eb907764c118dda95ee9f9dd026984
sofia <sofiapparreira@gmail.com>
2024-11-01 18:22:23 -0300

    tirando o redux

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,38 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux"; // Adicionado import do useSelector
 
 const NavBar = () => {
-  const { isLoggedIn, isLoading, user, role } = useSelector((state) => ({
-    isLoggedIn: state.auth.isLoggedIn,
-    isLoading: state.auth.isLoading,
-    user: state.auth.user?.nome,
-    role: state.auth.user?.role,
-  }));
-
   const NAVBAR_USER = [
+    { id: 1, path: "/", name: "Home" },
+
     {
       id: 1,
       path: "/",
       icon: "M253.3 35.1c6.1-11.8 1.5-26.3-10.2-32.4s-26.3-1.5-32.4 10.2L117.6 192 32 192c-17.7 0-32 14.3-32 32s14.3 32 32 32L83.9 463.5C91 492 116.6 512 146 512L430 512c29.4 0 55-20 62.1-48.5L544 256c17.7 0 32-14.3 32-32s-14.3-32-32-32l-85.6 0L365.3 12.9C359.2 1.2 344.7-3.4 332.9 2.7s-16.3 20.6-10.2 32.4L404.3 192l-232.6 0L253.3 35.1zM192 304l0 96c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-96c0-8.8 7.2-16 16-16s16 7.2 16 16zm96-16c8.8 0 16 7.2 16 16l0 96c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-96c0-8.8 7.2-16 16-16zm128 16l0 96c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-96c0-8.8 7.2-16 16-16s16 7.2 16 16z",
     },
+
     {
-      id: 2,
+      id: 1,
       path: "/",
       icon: "M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z",
     },
   ];
 
-  const NAVBAR_ADMIN = [
-    {
-      id: 1,
-      path: "/",
-      icon: "M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z",
-    },
-  ];
+  const NAVBAR_ADMIN = [{ id: 1, path: "/", name: "Home" }];
 
   return (
-    <nav className="bg-zinc-100 fixed z-20 w-full text-black flex justify-between px-8 2xl:px-32 py-6 items-center">
+    <nav class="bg-zinc-100 fixed z-20 w-full text-black flex justify-between px-8 2xl:px-32 py-6 items-center">
+      
       <ul className="flex md:gap-16 gap-8 items-center">
         <li>
           <Link to="/home">Home</Link>
@@ -40,39 +30,36 @@ const NavBar = () => {
       </ul>
 
       <ul className="flex gap-12 items-center">
-        {/* Links do usuário comum */}
-        {role !== "admin" &&
-          NAVBAR_USER.map((link) => (
-            <li key={link.id}>
-              <Link to={link.path}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="20"
-                  width="21.75"
-                  viewBox="0 0 576 512"
-                >
-                  <path className="fill-red-600" d={link.icon} />
-                </svg>
-              </Link>
-            </li>
-          ))}
-
-        {/* Links do administrador */}
-        {role === "admin" &&
-          NAVBAR_ADMIN.map((link) => (
-            <li key={link.id}>
-              <Link to={link.path}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="20"
-                  width="21.75"
-                  viewBox="0 0 576 512"
-                >
-                  <path className="fill-red-600" d={link.icon} />
-                </svg>
-              </Link>
-            </li>
-          ))}
+        <li>
+          <Link>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="20"
+              width="21.75"
+              viewBox="0 0 576 512"
+            >
+              <path
+                className="fill-red-600"
+                d="M253.3 35.1c6.1-11.8 1.5-26.3-10.2-32.4s-26.3-1.5-32.4 10.2L117.6 192 32 192c-17.7 0-32 14.3-32 32s14.3 32 32 32L83.9 463.5C91 492 116.6 512 146 512L430 512c29.4 0 55-20 62.1-48.5L544 256c17.7 0 32-14.3 32-32s-14.3-32-32-32l-85.6 0L365.3 12.9C359.2 1.2 344.7-3.4 332.9 2.7s-16.3 20.6-10.2 32.4L404.3 192l-232.6 0L253.3 35.1zM192 304l0 96c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-96c0-8.8 7.2-16 16-16s16 7.2 16 16zm96-16c8.8 0 16 7.2 16 16l0 96c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-96c0-8.8 7.2-16 16-16zm128 16l0 96c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-96c0-8.8 7.2-16 16-16s16 7.2 16 16z"
+              />
+            </svg>
+          </Link>
+        </li>
+        <li>
+          <Link to="/login">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="20"
+              width="20"
+              viewBox="0 0 512 512"
+            >
+              <path
+                className="fill-red-600"
+                d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"
+              />
+            </svg>
+          </Link>
+        </li>
       </ul>
     </nav>
   );
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,10 @@
-// main.jsx
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import './index.css';
-import App from './App.jsx';
-import store from './store.js'; // Certifique-se de que o caminho esteja correto
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
