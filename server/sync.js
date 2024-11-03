6ba90c4fa3e7053984baca8af59d898c94fcc4cb
sofia <sofiapparreira@gmail.com>
2024-11-03 08:51:15 -0300

    Feat: criação da tabela ItemCart

--- server/sync.js
+++ server/sync.js
@@ -1,6 +1,7 @@
 const sequelize = require('./config/database')
 const Product = require('./models/Product')
 const User = require('./models/User')
+const ItemCart = require('./models/User')
 const ProductImage = require('./models/ProductImage')
 
 sequelize.sync({ force: true }).then(() => {
