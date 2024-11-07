d2be4c0e3d7ba890416604df487b0c2f06041366
SOFIA_PARREIRA_PASSOS <12200131@OMEGAB34.BARROCA>
2024-11-07 07:56:15 -0300

    style: page payment

--- client/src/pages/user/Detail.jsx
+++ client/src/pages/user/Detail.jsx
@@ -38,7 +38,7 @@ const Detail = () => {
   return (
     <div>
       <NavBar />
-      <section className="flex min-h-screen justify-center items-center">
+      <section className="flex min-h-screen justify-center items-center pt-32">
         <div className="w-full mx-auto px-4 sm:px-6 lg:px-0">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-16 mx-auto max-md:px-2">
             <div className="img">
@@ -85,7 +85,7 @@ const Detail = () => {
                   <li>{product.power} CV</li>
                 </ul>
 
-                <div className="text-gray-500 text-base font-normal mt-12 border border-gray-200 rounded p-4">
+                <div className="text-gray-500 text-base font-normal mt-12 border border-gray-200 rounded p-4 mb-32">
                   {product.description
                     .split("\n")
                     .map((line, index) => (
--- server/config/database.js
+++ server/config/database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 const sequelize = new Sequelize ({
     dialect: 'sqlite',
-    storage: 'cardb',
+    storage: './config/cardb.sqlite',
 })
 
 module.exports = sequelize
