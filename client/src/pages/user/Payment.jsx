1f26defe1a659b00223bc886388896edc7825ddf
sofia <sofiapparreira@gmail.com>
2024-11-10 10:58:46 -0300

    detalhe de estilo da pagina payment

--- client/src/pages/user/Payment.jsx
+++ client/src/pages/user/Payment.jsx
@@ -296,11 +296,84 @@ const Payment = () => {
       )}
 
       {isPaid && (
+        
         <div className="py-8">
-          <Link className="mb-8 mt-8 underline mx-8" to="/home">
+            <Link className="mb-8 mt-8 underline mx-8" to="/home">
             Página inicial{" "}
           </Link>
 
+
+          <ol class="items-center flex w-full max-w-2xl text-center text-sm font-medium text-gray-500  sm:text-base mx-auto my-16">
+            <li class="after:border-1 flex items-center text-primary-700 after:mx-6 after:hidden after:h-1 after:w-full after:border-b after:border-gray-200 dark:text-primary-500 dark:after:border-gray-700 sm:after:inline-block sm:after:content-[''] md:w-full xl:after:mx-10">
+              <span class="flex items-center after:mx-2 after:text-gray-200 after:content-['/'] sm:after:hidden">
+                <svg
+                  class="me-2 h-4 w-4 sm:h-5 sm:w-5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M8.5 11.5 11 14l4-4m6 2a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                  />
+                </svg>
+                Carrinho
+              </span>
+            </li>
+
+            <li class="after:border-1 flex items-center  text-gray-500 text-primary-700 after:mx-6 after:hidden after:h-1 after:w-full after:border-b after:border-gray-200 dark:text-primary-500 dark:after:border-gray-700 sm:after:inline-block sm:after:content-[''] md:w-full xl:after:mx-10">
+              <span class="flex items-center after:mx-2 after:text-gray-200 after:content-['/'] sm:after:hidden">
+                <svg
+                  class="me-2 h-4 w-4 sm:h-5 sm:w-5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M8.5 11.5 11 14l4-4m6 2a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                  />
+                </svg>
+                Pagamento
+              </span>
+            </li>
+
+            <li class="flex shrink-0 items-center text-black">
+              <svg
+                class="me-2 h-4 w-4 sm:h-5 sm:w-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M8.5 11.5 11 14l4-4m6 2a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+              </svg>
+              Confirmação
+            </li>
+          </ol>
+
+
+        
           <div className="mt-4 flex flex-col items-center justify-center py-64">
             <p className="text-center text-gray-600 flex gap-2">
               Obrigado,{" "}
